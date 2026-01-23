6367ec21add35eb3ee0c33032e6034f0d229eabf
kehan <likehan217@gmail.com>
2026-01-23 15:05:52 +0800

    Add Level-5

--- src/main/java/Yuzu.java
+++ src/main/java/Yuzu.java
@@ -19,44 +19,62 @@ public class Yuzu {
                 break;
             }
 
-            if (input.equals("list")) {
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < count; i++) {
-                    System.out.println((i + 1) + "." + tasks[i]);
-                }
-            } else if (input.startsWith("mark")) {
-                int index = Integer.parseInt(input.split(" ")[1]) - 1;
-                tasks[index].markDone();
-                System.out.println("Nice! I've marked this task as done:\n  " + tasks[index]);
-            } else if (input.startsWith("unmark")) {
-                int index = Integer.parseInt(input.split(" ")[1]) - 1;
-                tasks[index].markUnDone();
-                System.out.println("OK, I've marked this task as not done yet:\n  " + tasks[index]);
-            } else {
-                // Create new tasks here: ToDo, Deadline, Event
-                Task newTask = null;
+            try {
+                if (input.equals("list")) {
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < count; i++) {
+                        System.out.println((i + 1) + "." + tasks[i]);
+                    }
+                } else if (input.startsWith("mark")) {
+                    int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                    tasks[index].markDone();
+                    System.out.println("Nice! I've marked this task as done:\n  " + tasks[index]);
+                } else if (input.startsWith("unmark")) {
+                    int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                    tasks[index].markUnDone();
+                    System.out.println("OK, I've marked this task as not done yet:\n  " + tasks[index]);
+                } else {
+                    // Create new tasks here: ToDo, Deadline, Event
+                    Task newTask = null;
 
-                if (input.startsWith("todo ")) {
-                    newTask = new ToDo(input.substring(5));
-                } else if (input.startsWith("deadline ")) {
-                    String[] parts = input.substring(9).split(" /by ");
-                    newTask = new Deadline(parts[0], parts[1]);
-                } else if (input.startsWith("event ")) {
-                    String[] parts = input.substring(6).split(" /from ");
-                    String[] timeParts = parts[1].split(" /to ");
-                    newTask = new Event(parts[0], timeParts[0], timeParts[1]);
-                }
+                    if (input.startsWith("todo")) {
+                        if (input.trim().equals("todo")) {
+                            throw new Exception("The description of a todo cannot be empty.");
+                        }
+                        newTask = new ToDo(input.substring(5));
+                    } else if (input.startsWith("deadline")) {
+                        if (input.trim().equals("deadline")) {
+                            throw new Exception("The description of a deadline cannot be empty.");
+                        }
+                        String[] parts = input.substring(9).split(" /by ");
+                        newTask = new Deadline(parts[0], parts[1]);
+                    } else if (input.startsWith("event")) {
+                        if (input.trim().equals("event")) {
+                            throw new Exception("The description of a event cannot be empty.");
+                        }
+                        String[] parts = input.substring(6).split(" /from ");
+                        String[] timeParts = parts[1].split(" /to ");
+                        newTask = new Event(parts[0], timeParts[0], timeParts[1]);
+                    } else {
+                        throw new Exception("I'm sorry, but I don't know what that means :-(\n");
+                    }
 
-                if (newTask != null) {
-                    tasks[count] = newTask;
-                    count++;
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println("  " + newTask);
-                    System.out.println("Now you have " + count + " tasks in the list.");
+                    if (newTask != null) {
+                        tasks[count] = newTask;
+                        count++;
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println("  " + newTask);
+                        System.out.println("Now you have " + count + " tasks in the list.");
+                    }
                 }
+                System.out.println("-------------------------------------");
+
+            } catch (Exception e) {
+                System.out.println(" OOPS!!! " + e.getMessage());
+                System.out.println("-------------------------------------");
             }
-            System.out.println("-------------------------------------");
         }
+
         scanner.close();
     }
 }
