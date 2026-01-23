dd44a6ea10c2e878115da6d2e7d8d39e3d76191a
kehan <likehan217@gmail.com>
2026-01-23 14:41:56 +0800

    Add Level-4

--- src/main/java/Yuzu.java
+++ src/main/java/Yuzu.java
@@ -2,72 +2,61 @@ import java.util.Scanner;
 
 public class Yuzu {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
-
-        String msg = "Hello! I'm Yuzu\n" +
-                "What can I do for you?\n" +
-                "-------------------------------------";
+        String msg = "Hello! I'm Yuzu\nWhat can I do for you?\n-------------------------------------";
         System.out.println(msg);
 
-        // Read in user input and say goodbye
         Scanner scanner = new Scanner(System.in);
         Task[] tasks = new Task[100];
         int count = 0;
 
-        // Read in until bye
         while (true) {
             String input = scanner.nextLine();
+            System.out.println("-------------------------------------");
 
-            // Used for bye
             if (input.equals("bye")) {
-                System.out.println("-------------------------------------\n" + "Bye. Hope to see you again soon!" + "\n-------------------------------------");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("-------------------------------------");
                 break;
             }
 
-            // List out the tasks
             if (input.equals("list")) {
-                System.out.println("-------------------------------------\n" + "Here are the tasks in your list:\n");
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < count; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    System.out.println((i + 1) + "." + tasks[i]);
                 }
-                System.out.println("-------------------------------------");
-                continue;
-            }
-
-            // Mark the task as done
-            if (input.startsWith("mark")) {
+            } else if (input.startsWith("mark")) {
                 int index = Integer.parseInt(input.split(" ")[1]) - 1;
                 tasks[index].markDone();
-
-                System.out.println("-------------------------------------");
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(tasks[index]);
-                System.out.println("-------------------------------------");
-                continue;
-            }
-
-            // Mark the task as undone
-            if (input.startsWith("unmark")) {
+                System.out.println("Nice! I've marked this task as done:\n  " + tasks[index]);
+            } else if (input.startsWith("unmark")) {
                 int index = Integer.parseInt(input.split(" ")[1]) - 1;
                 tasks[index].markUnDone();
+                System.out.println("OK, I've marked this task as not done yet:\n  " + tasks[index]);
+            } else {
+                // Create new tasks here: ToDo, Deadline, Event
+                Task newTask = null;
 
-                System.out.println("-------------------------------------");
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println(tasks[index]);
-                System.out.println("-------------------------------------");
-                continue;
+                if (input.startsWith("todo ")) {
+                    newTask = new ToDo(input.substring(5));
+                } else if (input.startsWith("deadline ")) {
+                    String[] parts = input.substring(9).split(" /by ");
+                    newTask = new Deadline(parts[0], parts[1]);
+                } else if (input.startsWith("event ")) {
+                    String[] parts = input.substring(6).split(" /from ");
+                    String[] timeParts = parts[1].split(" /to ");
+                    newTask = new Event(parts[0], timeParts[0], timeParts[1]);
+                }
+
+                if (newTask != null) {
+                    tasks[count] = newTask;
+                    count++;
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("  " + newTask);
+                    System.out.println("Now you have " + count + " tasks in the list.");
+                }
             }
-
-            tasks[count] = new Task(input);
-            count++;
-            System.out.println("-------------------------------------\nadded: " + input + "\n-------------------------------------");
+            System.out.println("-------------------------------------");
         }
-
         scanner.close();
     }
 }
