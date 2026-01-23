01cc544da94572af2beca72f7876fd84091f5565
kehan <likehan217@gmail.com>
2026-01-23 14:22:40 +0800

    Add Level-3

---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markDone() {
+        isDone = true;
+    }
+
+    public void markUnDone() {
+        isDone = false;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String toString() {
+        return "[" + (isDone ? "X" : " ") + "] " + description;
+    }
+}
--- src/main/java/Yuzu.java
+++ src/main/java/Yuzu.java
@@ -16,20 +16,22 @@ public class Yuzu {
 
         // Read in user input and say goodbye
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int count = 0;
 
         // Read in until bye
         while (true) {
             String input = scanner.nextLine();
 
+            // Used for bye
             if (input.equals("bye")) {
                 System.out.println("-------------------------------------\n" + "Bye. Hope to see you again soon!" + "\n-------------------------------------");
                 break;
             }
 
+            // List out the tasks
             if (input.equals("list")) {
-                System.out.println("-------------------------------------");
+                System.out.println("-------------------------------------\n" + "Here are the tasks in your list:\n");
                 for (int i = 0; i < count; i++) {
                     System.out.println((i + 1) + ". " + tasks[i]);
                 }
@@ -37,7 +39,31 @@ public class Yuzu {
                 continue;
             }
 
-            tasks[count]= input;
+            // Mark the task as done
+            if (input.startsWith("mark")) {
+                int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                tasks[index].markDone();
+
+                System.out.println("-------------------------------------");
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(tasks[index]);
+                System.out.println("-------------------------------------");
+                continue;
+            }
+
+            // Mark the task as undone
+            if (input.startsWith("unmark")) {
+                int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                tasks[index].markUnDone();
+
+                System.out.println("-------------------------------------");
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(tasks[index]);
+                System.out.println("-------------------------------------");
+                continue;
+            }
+
+            tasks[count] = new Task(input);
             count++;
             System.out.println("-------------------------------------\nadded: " + input + "\n-------------------------------------");
         }
