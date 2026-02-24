b508ae12c587ad8aee4cefcee1af3ef57e3fb6d4
kehan <likehan217@gmail.com>
2026-02-24 11:57:45 +0800

    Add comment for SnoozeCommand

--- src/main/java/yuzu/command/SnoozeCommand.java
+++ src/main/java/yuzu/command/SnoozeCommand.java
@@ -15,11 +15,26 @@ public class SnoozeCommand extends Command {
     private LocalDate newDate;
     private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("MMM d yyyy");
 
+    /**
+     * Construct a SnoozeCommand with the task index and new date.
+     *
+     * @param index   The task index.
+     * @param newDate The new LocalDate that the task should be postponed to.
+     */
     public SnoozeCommand(int index, LocalDate newDate) {
         this.index = index;
         this.newDate = newDate;
     }
 
+    /**
+     * Execute the snooze command by updating the target task's date.
+     * Tasks without date will not be able to execute this command
+     *
+     * @param tasks   The task list.
+     * @param ui      The UI to display the result message.
+     * @param storage The storage handler to save the updated task list.
+     * @return A string response of the snooze operation result.
+     */
     @Override
     public String execute(TaskList tasks, Ui ui, Storage storage) {
         assert tasks != null : "TaskList cannot be null for snooze";
